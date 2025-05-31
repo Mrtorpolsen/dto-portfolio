de5c2f2d6bd20f38e69997ae3d7ebbcff6bc7325
Torp <mrtorpolsen@gmail.com>
2025-05-31 13:40:11 +0200

    update/ styling for desktop

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -14,11 +14,29 @@ export default function Projects() {
     },
   ];
 
-  const listImages = projects.map((project) => (
+  const listImages = projects.map((project, index) => (
     <li key={project.src}>
-      <div className="sm:grid sm:grid-cols-[60%_40%]">
-        <img src={project.src} alt={project.alt} />
-        <div className="mx-8 bg-white/80 p-3 relative -mt-6 sm:mt-0 sm:p-0 sm:bg-white/0">
+      <div
+        className={`sm:grid ${
+          index % 2 === 0 ? "sm:grid-cols-[60%_40%]" : "sm:grid-cols-[40%_60%]"
+        }`}
+      >
+        <img
+          className={`${
+            index % 2 === 0
+              ? "col-start-1 row-start-1"
+              : "col-start-2 row-start-1"
+          }`}
+          src={project.src}
+          alt={project.alt}
+        />
+        <div
+          className={` mx-8 bg-white/80 p-3 relative -mt-6 sm:mt-0 sm:p-0 sm:bg-white/0${
+            index % 2 === 0
+              ? "col-start-1 row-start-1"
+              : "col-start-2 row-start-1"
+          }`}
+        >
           <h2 className="text-xl font-bold text-center mb-2">
             {project.heading}
           </h2>
@@ -28,7 +46,7 @@ export default function Projects() {
     </li>
   ));
   return (
-    <section className="h-screen relative z-1" id="projects">
+    <section className="relative z-1" id="projects">
       <div className="flex w-full justify-center">
         <h2 className="text-3xl my-10 font-bold">Projects</h2>
       </div>
